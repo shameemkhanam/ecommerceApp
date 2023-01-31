0cd855671343b0d3e48e0318688bbf45cb945335
unknown <shameemkhanam@gmail.com>
2023-01-31 23:02:55 +0530

    first commit

---
+++ src/app/products/view-product-by-category/view-product-by-category.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-product-by-category',
+  templateUrl: './view-product-by-category.component.html',
+  styleUrls: ['./view-product-by-category.component.css']
+})
+export class ViewProductByCategoryComponent {
+
+}
---
+++ src/app/products/view-product-by-date/view-product-by-date.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-product-by-date',
+  templateUrl: './view-product-by-date.component.html',
+  styleUrls: ['./view-product-by-date.component.css']
+})
+export class ViewProductByDateComponent {
+
+}
